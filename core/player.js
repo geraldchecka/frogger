2adcb9eb7a37901a18a478a2224591acb0f2f0b5
Anvesh Checka <anvesh.checka@gmail.com>
2020-12-28 22:01:03 +0530

    Added player class and method to capture keyboard events

--- core/player.js
+++ core/player.js
@@ -1,5 +1,49 @@
-export default function Player(context, config) {
-  this.context = context;
-  this.row = config.characterInitialise.row;
-  this.col = config.characterInitialise.col;
+function resolveKey(e) {
+  let code = "invalid";
+
+  let keyConstants = {
+    key: {
+      "ArrowLeft": "left",
+      "ArrowUp": "up",
+      "ArrowRight": "right",
+      "ArrowDown": "down",
+    },
+    keyCode: {
+      37: "left",
+      38: "up",
+      39: "right",
+      40: "bottom",
+    }
+  };
+
+  if (e.key !== undefined) {
+    code = keyConstants.key[e.key] ?? code;
+  }
+  else if (e.keyCode !== undefined) {
+    code = keyConstants.keyCode[e.keyCode] ?? code;
+  }
+  return code;
 }
+
+export default function Player(model) {
+  function keyHandler(e) {
+    if (e.defaultPrevented) {
+      return;
+    }
+
+    let key = resolveKey(e);
+
+    console.log(key);
+  }
+
+  document.addEventListener(
+    "keyup",
+    keyHandler,
+    true          // Because we'd want to capture it ASAP
+  );
+
+  // this.movePlayer = function() {};
+  this.updateModel = function() {
+    model.setPosition(0,0);
+  };
+};
